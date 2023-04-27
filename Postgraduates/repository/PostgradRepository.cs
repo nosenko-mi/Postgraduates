65f94aa14323cc697bc13719ff21868b6dcd5607
nickolay <nosenkomi02@gmail.com>
2023-04-27 12:46:59 +0300

    refactor

--- Postgraduates/repository/PostgradRepository.cs
+++ Postgraduates/repository/PostgradRepository.cs
@@ -23,57 +23,6 @@ namespace Postgraduates.repository
 
         public IEnumerable<Postgrad>? RunQuery(string[] queryParams)
         {
-            //XDocument xml = XDocument.Load(FilePath); // Load the XML document
-            //IEnumerable<XElement> query = new List<XElement>();
-            //IEnumerable<Postgrad> list = new List<Postgrad>();
-            //int val;
-            //if (queryParams[0].StartsWith('>'))
-            //{
-            //    try
-            //    {
-            //        val = int.Parse(queryParams[0].Substring(1, queryParams[0].Length - 1));
-            //        int v = 0;
-            //        query =
-            //            from postgrad in xml.Descendants("Postgrad")
-            //            where ParseString(postgrad.Element(queryParams[1])) > val
-            //            select postgrad;
-            //    } catch (Exception ex)
-            //    {
-            //        MessageBox.Show(ex.Message);
-            //    }
-            //}
-            //else if (queryParams[0].StartsWith('<'))
-            //{
-            //    try
-            //    {
-            //        val = int.Parse(queryParams[0].Substring(1, queryParams[0].Length-1));
-            //        query =
-            //            from postgrad in xml.Descendants("Postgrad")
-            //            where ParseString(postgrad.Element(queryParams[1])) < val
-            //            select postgrad;
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show(ex.Message);
-            //    }
-            //}
-            //else
-            //{
-            //    try
-            //    {
-            //        query =
-            //            from postgrad in xml.Descendants("Postgrad")
-            //            where (string)postgrad.Element(queryParams[1]) == queryParams[0]
-            //            select postgrad;
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        MessageBox.Show(ex.Message);
-            //    }
-            //}
-            //var converter = new PostgradConverter();
-            //list = converter.ToList(query);
-
             var context = new QueryContext();
             if (queryParams[0].StartsWith('>'))
             {
@@ -92,18 +41,5 @@ namespace Postgraduates.repository
             return list;
         }
 
-        private int ParseString(XElement element)
-        {
-            try
-            {
-                return int.Parse(element.Value);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-                return -1;
-            }
-
-        }
     }
 }
